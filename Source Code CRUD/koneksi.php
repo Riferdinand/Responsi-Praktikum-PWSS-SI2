b348f4f2ea5ae66982d8e2c59bb7ce0ae1e9263f
Riferdinand <113415789+Riferdinand@users.noreply.github.com>
2023-12-28 09:59:00 +0700

    Update koneksi.php

--- Source Code CRUD/koneksi.php
+++ Source Code CRUD/koneksi.php
@@ -10,7 +10,7 @@ if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 
-$query = "CREATE TABLE IF NOT EXISTS mahasiswa (
+$query = "CREATE TABLE IF NOT EXISTS kelas (
             nim char(10) PRIMARY KEY,
             nama char(40),
             tanggal_lahir date,
